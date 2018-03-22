7244cadf9430c5b65b9502c20564016e9b328ae7
Johnn Mis <inrumi@gmail.com>
2018-03-22 18:31:24 -0500

    contadores

--- webpack.config.js
+++ webpack.config.js
@@ -2,60 +2,58 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const config = {
-    entry: {
-      app: './src/js/app.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'public/dist'),
-      publicPath: 'public/'
-    },
-    devtool: 'source-map',
-    devServer: {
-      contentBase: 'public/dist',
-      watchContentBase: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)?$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env']
-            }
-          }
-        },
-        {
-          test: /\.(scss)$/,
-          use: ExtractTextPlugin.extract({
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  minimize: true
-                }
-              },
-              {
-                loader: 'postcss-loader', options: {
-                  plugins() {
-                    return [require('autoprefixer')({browsers: 'last 3 versions'})]
-                  }
-                }
-              },
-              'sass-loader'
-            ]
-          })
-        }
-      ]
-    },
-    plugins: [
-      new ExtractTextPlugin('styles.css'),
-      // new UglifyJsPlugin(),
-    ]
+const javascript = {
+  test: /\.(js)?$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['env']
+    }
   }
+};
+
+const scss = {
+  test: /\.(scss)$/,
+  use: ExtractTextPlugin.extract({
+    use: [
+      {
+        loader: 'css-loader',
+        options: {
+          url: false,
+          minimize: true
+        }
+      },
+      {
+        loader: 'postcss-loader', options: {
+          sourceMap: true,
+          plugins() {
+            return [require('autoprefixer')({browsers: 'last 3 versions'})]
+          }
+        }
+      },
+      'sass-loader?sourceMap'
+    ]
+  })
+};
+
+const config = {
+      entry: {
+        app: './src/js/app.js'
+      },
+      output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public/dist'),
+      },
+      devtool: 'source-map',
+      module: {
+        rules: [scss, javascript]
+      },
+      plugins: [
+        new ExtractTextPlugin('styles.css'),
+        // new UglifyJsPlugin(),
+      ]
+    }
 ;
 
 module.exports = config;
