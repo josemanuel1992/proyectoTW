0396082bbb71a5dd52be6b2be13b1ffb8283ad08
Josh <zsweetcore@gmail.com>
2018-04-18 00:54:44 -0600

    Casi
    falta visualizarse

---
+++ public/admin/includes/funcionesNos.php
@@ -0,0 +1,72 @@
+<?php
+require_once("_db.php");
+if(!isset($_POST['accion'])){
+    $accion ="";
+}else{
+    $accion = $_POST['accion'];
+}
+switch ($accion) {
+    case 'eliminarTitulo': eliminarTitulos();
+        break;
+
+    case 'insertar' : insertarNosotros();
+        break;
+
+    case 'consultar' : consultarTitul();
+        break;
+
+    case 'individual': individualNos();
+        break;
+
+    case 'editar': editarNos();
+        break;
+
+    default:
+        # code...
+        break;
+}
+
+function editarNos(){
+    global $db;
+    print_r($_POST);
+    extract($_POST);
+    $db->update("nosotros",[
+        "titulo_nosotrs" => $titulo,
+        "subtitulo_nosotrs" => $subtitulo,
+        "texto_nosotrs" => $texto
+    ], ["id_nosotrs" => $id]);
+
+    echo "Se ha actualizado correctamente el apartado con ID ".$id;
+}
+function insertarNosotros(){
+    global $db;
+    extract($_POST);
+    $db->insert("nosotros",[
+        "titulo_nosotrs" => $titulo,
+        "subtitulo_nosotrs" => $subtitulo,
+        "texto_nosotrs" => $texto,
+        "imagen_nosotrs" => $imagenOculta
+    ]);
+    $titul = $db->id();
+    echo "Se ha añadido correctamente el apartado ".$titul;
+}
+function consultarTitul(){
+    global $db;
+    $nosotros = $db->select("nosotros","*");
+    echo json_encode($nosotros);
+}
+function individualNos(){
+    global $db;
+    extract($_POST);
+    $nosotros = $db->get("nosotros","*", ["id_nosotrs"=> $id
+    ]);
+    echo json_encode($nosotros);
+}
+function eliminarTitulos(){
+    global $db;
+    extract($_POST);
+    $nosotros = $db->delete("nosotros",["id_nosotrs" => $titul]);
+    echo "Se ha eliminado el apartado";
+}
+
+?>
